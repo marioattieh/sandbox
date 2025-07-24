0bc7924a3dae31df14926efa4c1938d1151cd9ac
Mario Attieh <mario.a@eurisko.net>
2025-07-24 11:27:14 +0300

    Add dynamic alias in vite config
    - Use aliases from tsconfig.app.json to generate aliases in Vite config to avoid repetition and confusion

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,25 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vitest/config";
 
+import { compilerOptions } from "./tsconfig.app.json";
+
+const getAlias = () =>
+  Object.entries(compilerOptions.paths).reduce(
+    (acc: Record<string, string>, [key, value]) => {
+      const newKey = key.slice(0, key.length - 2);
+      const newValue = `./${value[0].slice(0, value[0].length - 2)}`;
+
+      acc[newKey] = path.resolve(__dirname, newValue);
+
+      return acc;
+    },
+    {},
+  );
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
-    },
-  },
+  resolve: { alias: getAlias() },
   test: {
     globals: true,
     environment: "jsdom",
